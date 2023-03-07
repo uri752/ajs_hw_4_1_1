9ae85a91c1bc2f03070190dcc067dcaf6dc63238
uri752 <uri752blag@mail.ru>
2023-03-07 20:52:44 +0300

    init commit

---
+++ src/__test__/test.js
@@ -0,0 +1,25 @@
+// Реализуйте функцию, которая на вход принимает объект вида: {name: 'Маг', health: 90}
+// И возвращает ответ в виде одной из строк: healthy, wounded, critical
+// Здоровье более 50 - зелёный;
+// Здоровье от 50 и до 15 - жёлтый;
+// Менее 15 - красный.
+
+import  { getHealth } from '../getHealth'
+
+test('getHealth test healthy', () => {
+    const hero =  {name: 'Маг', health: 90};
+    const result = getHealth(hero);
+    expect(result).toBe('healthy');
+})
+
+test('getHealth test wounded', () => {
+    const hero =  {name: 'Маг', health: 40};
+    const result = getHealth(hero);
+    expect(result).toBe('wounded');
+})
+
+test('getHealth test critical', () => {
+    const hero =  {name: 'Маг', health: 10};
+    const result = getHealth(hero);
+    expect(result).toBe('critical');
+})
---
+++ src/getHealth.js
@@ -0,0 +1,18 @@
+// Реализуйте функцию, которая на вход принимает объект вида: {name: 'Маг', health: 90}
+// И возвращает ответ в виде одной из строк: healthy, wounded, critical
+// Здоровье более 50 - зелёный;
+// Здоровье от 50 и до 15 - жёлтый;
+// Менее 15 - красный.
+
+export function getHealth(hero) {
+    let result = 'unknown';
+
+    if (hero.health > 50) {
+        result = 'healthy';
+    } else if (hero.health > 15) {
+        result = 'wounded';
+    } else {
+        result = 'critical';
+    }
+    return result;
+}
